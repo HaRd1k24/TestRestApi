7313c6739536e7a00c93f937d919060d9c02c219
Lev Trapeznikov <leva.trapeznikov@mail.ru>
2021-05-19 17:55:06 +0300

    NotResult

--- src/test/java/newsapi/BDDTest.java
+++ src/test/java/newsapi/BDDTest.java
@@ -12,14 +12,13 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static io.restassured.RestAssured.given;
 
 public class BDDTest {
 
-    public static final String API_KEY = "82edc8x7933e14dc38b65bc307bc86b33";
+    public static final String API_KEY = "82edc87933e14dc38b65bc307bc86b33";
     public static RequestSpecification spec;
 
     @BeforeAll
@@ -34,51 +33,46 @@ public class BDDTest {
         spec = builder.build();
     }
 
-
     @Test
     @DisplayName("Статьи теслы за послдений месяц")
     void stateTeslaOnMonth() {
         String str = "https://newsapi.org/v2/everything?q=tesla&from=2021-04-19&sortBy=publishedAt&apiKey=82edc87933e14dc38b65bc307bc86b33";
 
         Response response = RestAssured.given().spec(spec)
+                .queryParam("from", "2021-04-19")
                 .queryParam("sortBy", "publishedAt")
                 .when().get("?q=tesla");
 
         JsonPath jsonPath = response.jsonPath();
-
-        List<String> list = new ArrayList<>(jsonPath.get("articles"));
-        System.out.println(list);
-        // Assertions.assertTrue(list.contains("a"));
-
+        List<Object> title = jsonPath.getList("articles.title");
+        title.forEach(System.out::println);
+        Assertions.assertFalse(title.isEmpty());
     }
-
 
     @Test
     @DisplayName("Статьи с упоминанием Apple за вчерашний день")
     public void stateApple() {
         Response response = RestAssured.given().spec(spec)
-
+                .queryParam("from", "2021-05-18")
+                .queryParam("to", "2021-05-18")
                 .queryParam("sortBy", "popularity")
                 .when().get("?q=apple");
+        //https://newsapi.org/v2/everything?q=apple&from=2021-05-18&to=2021-05-18&
+        // sortBy=popularity&apiKey=82edc87933e14dc38b65bc307bc86b33
 
         JsonPath jsonPath = response.jsonPath();
-        System.out.println(jsonPath.get("status").toString());
-        String str = jsonPath.get("status");
-        Assertions.assertTrue(str.contains("ok"));
 
-
+        List<Object> list = jsonPath.getList("articles.author");
+        list.forEach(System.out::println);
     }
 
     @Test
     @DisplayName("Все статьи о биткойнах")
     void articleBitcoin() {
-
-      //  String str = "https://newsapi.org/v2/everything?q=bitcoin&apiKey=82edc87933e14dc38b65bc307bc86b33";
+        //  String str = "https://newsapi.org/v2/everything?q=bitcoin&apiKey=82edc87933e14dc38b65bc307bc86b33";
         given().spec(spec)
                 .when().get("q=bitcoin")
                 .then().log().all()
                 .header("Server", Matchers.equalTo("cloudflare"));
-
-
     }
 }
